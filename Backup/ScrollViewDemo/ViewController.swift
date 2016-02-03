8332e72ad6f74e6e3f32e168584138808442ddb9
CHENG LI <chengli@CHENGdeMacBook-Pro.local>
2016-02-03 15:32:58 +0800

    add backup

---
+++ Backup/ScrollViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScrollViewDemo
+//
+//  Created by CHENG LI on 16/2/3.
+//  Copyright © 2016年 CHENG LI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
